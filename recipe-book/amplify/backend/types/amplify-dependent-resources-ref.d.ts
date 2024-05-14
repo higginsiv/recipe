fbf9be63bb0d51a6dfd13f44bee9b2c5acc7e35e
William Higgins <higginsiv@gmail.com>
2024-05-13 21:54:40 -0400

    feat(): added amplify to the project

---
+++ recipe-book/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "recipebook7d1b8533": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
